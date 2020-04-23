865128cd6483ad1d0ff2a61784ccc2996d66d86e
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-23 17:25:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def execute_transaction
     @receiver.balance += @amount
     @status = "complete"
   else
-    @status == "rejected"
+    @status = "rejected"
     return "Transaction rejected. Please check your account balance."
   end
 end
