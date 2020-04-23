20369ac6c629e73ed60a8495ea6f02f4caf5c0c2
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-23 10:41:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 attr_reader :status
-attr_accessor :sender, :receiver, :transfer_amount
+attr_accessor :sender, :receiver, :amount
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
